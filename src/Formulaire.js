b76405c6dfbb45a691443cc72f2326d8d96c7fd5
marwaouahouah1 <84792182+marwaouahouah1@users.noreply.github.com>
2022-12-01 22:59:28 +0100

    v1

--- src/Formulaire.js
+++ src/Formulaire.js
@@ -1,11 +1,21 @@
 import React, {useState} from 'react';
-
+import './Formulaire.css';
 function Formulaire(){
 
 	return(
-    <div className='app'>
-        <span>coucou Molly's squad!</span>
+    <><div className='app'>
+            <title>Welcome to Ditto's formu</title>
+        <form class="myForm" method="get">
+            <label>Name</label>
+            <input type="text" name="customer_name" required/>
+            <label>Phone</label>
+            <input type="tel" name="phone_number"/>
+            <label>Email</label>
+            <input type="email" name="email_address"/>
+            <p><button>Submit Booking</button></p>
+        </form>
        </div>
+       </>
 	)
   
 }
