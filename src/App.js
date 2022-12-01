4171db3f245820b58c70690096a94efe56fe51ed
marwaouahouah1 <84792182+marwaouahouah1@users.noreply.github.com>
2022-12-01 22:29:42 +0100

    go
    go

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Formulaire from './Formulaire'
+import './App.css';
+
+function App() {
+  return (
+     <Formulaire/>
+  );
+}
+export default App;
---
+++ src/Formulaire.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+function Formulaire(){
+
+	return(
+    <div className='app'>
+        <span>coucou Molly's squad!</span>
+       </div>
+	)
+  
+}
+
+export default Formulaire;
