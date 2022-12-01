f7e022503affeba4d92f91c513b09ea8c7cbb808
marwaouahouah1 <84792182+marwaouahouah1@users.noreply.github.com>
2022-12-02 00:48:07 +0100

    v2

--- src/Formulaire.js
+++ src/Formulaire.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { useForm } from "react-hook-form";
 import { Form, Button } from 'semantic-ui-react';
 
 import './Formulaire.css';
-
+import ditto from './dittoGif.gif';
 function Formulaire(){
     const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = (data) => {
@@ -11,53 +11,62 @@ function Formulaire(){
     }
     return (
         <div>
-            <Form onSubmit={handleSubmit(onSubmit)}>
-                <Form.Field>
-                    <label>Prenom</label>
-                    <input type="text" 
-                        name="firstName" 
-                        placeholder='Prénom'
-                        {...register("firstName", { required: true, maxLength: 10 })}
-                    />
-                    
-                </Form.Field>
-                {errors.firstName && <p>Renseignez votre prénom !</p>}
-                <Form.Field>
-                    <label>Nom de famille</label>
-                    <input
-                        placeholder='Nom de famille'
-                        type="text"
-                        name="lastName"
-                        {...register("lastName", { required: true, maxLength: 10 })}
-                    />
-                </Form.Field>
-                {errors.lastName && <p>Renseignez votre nom de famille !</p>}
-                <Form.Field>
-                    <label>Email</label>
-                    <input
-                        placeholder='Email'
-                        type="email"
-                        name="email_address"
-                        {...register("email",
-                            {
-                                required: true,
-                                pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                            })}
-                    />
-                </Form.Field>
-                {errors.email && <p>Renseignez votre email !</p>}
-                <Form.Field>
-                    <label>Phone</label>
-                            <input 
-                                type="tel" 
-                                name="phoneNumber"
-                                placeholder='Numéro de téléphone'
-                                {...register("phoneNumber", { required: true, maxLength: 10 })}
-                            />
-                </Form.Field>
-                {errors.phoneNumber && <p>Renseignez votre numéro de téléphone</p>}
-                <Button type='submit'>Valider</Button>
-            </Form>
+            <div class="app">
+                <div class="ditto">
+                <centre><span>Welcome to Ditto's form</span></centre><br></br>
+                </div>
+                <div class="form">
+                    <div class="formulaire">
+                    <Form onSubmit={handleSubmit(onSubmit)}>
+                        <Form.Field>
+                            <label>Prenom   </label>
+                            <input type="text" 
+                                name="firstName" 
+                                placeholder='Prénom'
+                                {...register("firstName", { required: true, maxLength: 10 })}
+                            /><br></br><br></br>
+                            
+                        </Form.Field>
+                        {errors.firstName && <p>Renseignez votre prénom !</p>}
+                        <Form.Field>
+                            <label>Nom   </label>
+                            <input
+                                placeholder='Nom de famille'
+                                type="text"
+                                name="lastName"
+                                {...register("lastName", { required: true, maxLength: 10 })}
+                            /><br></br><br></br>
+                        </Form.Field>
+                        {errors.lastName && <p>Renseignez votre nom de famille !</p>}
+                        <Form.Field>
+                            <label>Email   </label>
+                            <input
+                                placeholder='Email'
+                                type="email"
+                                name="email_address"
+                                {...register("email",
+                                    {
+                                        required: true,
+                                        pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                                    })}
+                            /><br></br><br></br>
+                        </Form.Field>
+                        {errors.email && <p>Renseignez votre email !</p>}
+                        <Form.Field>
+                            <label>Phone   </label>
+                                    <input 
+                                        type="tel" 
+                                        name="phoneNumber"
+                                        placeholder='Numéro de téléphone'
+                                        {...register("phoneNumber", { required: true, maxLength: 10 })}
+                                    /><br></br><br></br>
+                        </Form.Field>
+                        {errors.phoneNumber && <p>Renseignez votre numéro de téléphone</p>}
+                        <img class="d" src={ditto} alt='hello'/> 
+                    </Form>
+                    </div>
+                </div>
+            </div>
         </div>
     )
   
